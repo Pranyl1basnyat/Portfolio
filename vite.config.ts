a7455d94eee64ff737e4f8f3b80bd7d1292b3898
Pranil Basnyat <pranylbasnyat@gmail.com>
2026-05-11 15:17:38 +0545

    Initial portfolio deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Asset-Linker/',     // ← Very Important
+  base: '/',
 
   plugins: [
     react(),
